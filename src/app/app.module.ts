f2a9ade4f30459087d797f4702b3045a1c94d166
fcakiroglu16 <f-cakiroglu@outlook.com>
2022-06-23 12:56:09 +0300

    pipes konusu tamamlandı.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { NgforComponent } from './directives/ngfor/ngfor.component';
-import { NgIfComponent } from './directives/ng-if/ng-if.component';
-import { NgSwitchComponent } from './directives/ng-switch/ng-switch.component';
-import { NgClassComponent } from './directives/ng-class/ng-class.component';
-import { NgStyleComponent } from './directives/ng-style/ng-style.component';
-import { NgModelComponent } from './directives/ng-model/ng-model.component';
-import { CustomDirective } from './directives/custom.directive';
+import { PipeComponent } from './pipes/pipe/pipe.component';
+import { UpperPipe } from './pipes/upper.pipe';
 
 
 
+//Component-Directive-Pipe
 @NgModule({
   declarations: [
     AppComponent,
-    NgforComponent,
-    NgIfComponent,
-    NgSwitchComponent,
-    NgClassComponent,
-    NgStyleComponent,
-    NgModelComponent,
-    CustomDirective,
-   
- 
+    PipeComponent,
+    UpperPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
