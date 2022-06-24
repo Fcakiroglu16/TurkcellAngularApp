f36d10a0afa29d1b82931a4012413e9417749c13
fcakiroglu16 <f-cakiroglu@outlook.com>
2022-06-24 17:24:46 +0300

    radio buttons tamamlandı.

--- src/app/forms/reactive-type2/reactive-type2.component.ts
+++ src/app/forms/reactive-type2/reactive-type2.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CountryDropMenu } from 'src/app/models/country-drop-menu';
+import { GenderRadioMenu } from 'src/app/models/gender-radio-menu';
 
 @Component({
   selector: 'app-reactive-type2',
@@ -11,13 +12,15 @@ export class ReactiveType2Component implements OnInit {
 
   loginForm:FormGroup;
   countryMenu: CountryDropMenu[] = [{ text: "Seçiniz", value: 0 }, { text: "Türkiye", value: 1 }, { text: "Almanya", value: 2 }, { text: "Fransa", value: 3 }];
+  genderMenu: GenderRadioMenu[] = [{ text: "Erkek", value: 1 }, { text: "Kadın", value: 2 }]
   constructor() {
 
     this.loginForm= new FormGroup({
       userName:new FormControl(),
       password:new FormControl(),
       rememberMe:new FormControl(false),
-      country:new FormControl(0)
+      country:new FormControl(0),
+      gender:new FormControl(2)
      
      })
 
--- src/app/forms/reactive/reactive.component.ts
+++ src/app/forms/reactive/reactive.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { setupTestingRouterInternal } from '@angular/router/testing';
 import { CountryDropMenu } from 'src/app/models/country-drop-menu';
+import { GenderRadioMenu } from 'src/app/models/gender-radio-menu';
 
 @Component({
   selector: 'app-reactive',
@@ -12,17 +13,21 @@ export class ReactiveComponent implements OnInit {
 
   loginForm: FormGroup;
   countryMenu: CountryDropMenu[] = [{ text: "Seçiniz", value: 0 }, { text: "Türkiye", value: 1 }, { text: "Almanya", value: 2 }, { text: "Fransa", value: 3 }];
+
+  genderMenu: GenderRadioMenu[] = [{ text: "Erkek", value: 1 }, { text: "Kadın", value: 2 }]
   constructor(fb: FormBuilder) {
 
     this.loginForm = fb.group({
       userName: '', // FormControl();
       password: '',
       rememberMe:false,
-      country:0
+      country:0,
+      gender:[1]
     })
   }
 
   ngOnInit(): void {
+  
   }
   signin()
   {
